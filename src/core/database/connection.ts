3e3e30f4795a4ff9a9bede3df950d64c800b6630
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-11-10 00:19:59 -0300

    refactor(connection): switch from mongodb lib to mongoose library

--- src/core/database/connection.ts
+++ src/core/database/connection.ts
@@ -1,12 +1,9 @@
-import { MongoClient } from "mongodb";
+import mongoose from "mongoose"; 
 import  { env } from "../../config/env";
-
-let client: MongoClient;
 
 export async function connectDB() {
   try {
-    client = new MongoClient(env.bdUri);
-    await client.connect();
+    await mongoose.connect(env.bdUri);
     console.log("✅ Conectado ao MongoDB com sucesso!");
   } catch (error) {
     console.error("❌ Erro ao conectar ao MongoDB:", error);
